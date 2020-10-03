524b2e87495da3eb9bf8e8146e67ab4c9f0ca971
Olena Sazonova <30494770+olena-sazonova@users.noreply.github.com>
2020-10-03 23:58:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == underfined || Array.isArray(array) === false){
+    return 0;
+  } 
+
+  var m = array[0];
+    for (let i = 0; i < array.length; i++){
+      if (array[i] < m){
+        m = array[i];
+      }
+    }
+    return m;     
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == underfined || Array.isArray(array) === false){
+    return 0;
+  } 
+  var n = array[0];
+  for (let i = 0; i < array.length; i++){
+    if (array[i] > n){
+      n = array[i];
+    }
+  }
+  return n;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+
+  if (array == underfined || Array.isArray(array) === false){
+    return 0;
+  } 
+
+  var s = 0;
+  var a = 0;
+for (let i = 0; i < array.length; i++){
+    s = s + array[i];
+  }
+  a = s / array.length;
+  return a;
 }
+
